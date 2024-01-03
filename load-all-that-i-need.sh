fe63bb99b780b31e40726de0543e88230294e154
moraeskkj <mooraesz123@gmail.com>
2024-01-03 13:12:36 +0000

    looks good

--- load-all-that-i-need.sh
+++ load-all-that-i-need.sh
@@ -12,7 +12,6 @@ banner()
 banner "Updating and upgrading your packages"
 sudo pacman -Syyu
 
-
 banner "Installing pyenv, python and pip3"
 curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
@@ -23,13 +22,15 @@ pyenv install 3.12.0
 pyenv global 3.12.0
 exec $SHELL
 currentpython=$(which python)
-if [ $currentpython="/home/akame/.pyenv/shims/python"
+if [ $currentpython="/home/akame/.pyenv/shims/python" ]
 then
-    echo "Python is sucessfuly installed"
+    echo "Python was installed correctly"
     pacman -S python-pip
+    pip3 install --upgrade pip
 else
     echo "Please check your pyenv installation after"
 
+banner "Installing i3, alacritty and fish"
 sudo pacman -S i3
 sudo pacman -S alacritty 
 sudo pacman -S fish
@@ -40,141 +41,14 @@ chsh -s /usr/local/bin/fish
 set -Ux PYENV_ROOT $HOME/.pyenv
 fish_add_path $PYENV_ROOT/bin
 
+cd /akame/home/
+mkdir .config
+cd .config
+git clone https://github.com/moraeskkj/dotfiles.git
+rm -rf arch-ricing.png picom qtile README.md .gitignore
 
-i3 -> alacritty -> fish shell -> reconfigure pyenv in fish -> curl all your config files -> obsidian ->
-
+echo "Please don't forget to make sure that all config are correctly"
 
 mkdir /akame/scripts
 mkdir /akame/tools
 
-#echo
-#echo 'INSTALANDO O GOLANG'
-#Acessar a URL https://go.dev/doc/install e verificara versao mais atualizada
-#wget https://go.dev/dl/go1.18.4.linux-amd64.tar.gz
-#rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.4.linux-amd64.tar.gz
-#export PATH=$PATH:/usr/local/go/bin
-#go version
-#apt install -y golang-go
-
-#pip3
-echo
-echo 'INSTALANDO O PIP3'
-apt-get install -y python3-pip
-pip3 --version
-echo
-
-#snapd
-echo 'INSTALANDO O SNAP'
-apt install -y snapd
-echo
-
-########################################################################
-
-
-echo '##############################################'
-echo '#                                            #'
-echo '#         INSTALANDO AS FERRAMENTAS                  #'
-echo '#                                            #'
-echo '##############################################'
-echo
-
-
-#sqlmap
-echo 'INSTALANDO SQLMAP'
-apt install sqlmap -y
-echo
-
-#Nmap
-echo 'INSTALANDO Nmap'
-sudo apt-get install nmap
-echo
-
-#dirb
-echo 'INSTALANDO DIRB'
-sudo apt install dirb
-echo
-
-#gobuster
-echo 'INSTALANDO GOBUSTER'
-sudo apt install gobuster
-echo
-
-#wAFW00F
-echo 'INSTALANDO WAFW00F'
-sudo apt install wafw00f
-echo
-
-#hashcat
-echo 'INSTALANDO HASHCAT'
-sudo apt install hashcat
-echo
-
-####################################################################
-
-echo '##############################################'
-echo '#                                            #'
-echo '#          FERRAMENTAS UTILIZANDO O GIT          #'
-echo '#                                            #'
-echo '##############################################'
-echo
-
-#sublist3r
-echo 'INSTALANDO SUBLISTER'
-git clone https://github.com/aboul3la/Sublist3r.git
-cd Sublist3r
-pip3 install -r requirements.txt
-cd /root/
-echo
-
-#testssl
-
-echo 'INSTALANDO TOOL TEST/SSL'
-git clone https://github.com/drwetter/testssl.sh.git
-echo
-
-#clickjacking
-echo 'INSTALANDO TOOL CLICKJACKING'
-git clone https://github.com/sensepost/jack.git
-echo
-
-################################################################
-
-echo '##############################################'
-echo '#                                            #'
-echo '#             FERRAMENTAS UTILIZANDO GO          #'
-echo '#                                            #'
-echo '##############################################'
-echo
-
-#ffuf
-echo 'INSTALANDO FFUF'
-go install github.com/ffuf/ffuf/v2@latest
-mv /root/go/bin/* /usr/bin
-echo
-
-###########################################################################
-
-echo '##############################################'
-echo '#                                            #'
-echo '#                BAIXANDO WORDLIST               #'
-echo '#                                            #'
-echo '##############################################'
-echo
-
-#WORDLIST
-cd /root/Wordlist
-wget https://wordlists-cdn.assetnote.io/data/manual/raft-large-extensions.txt
-wget https://wordlists-cdn.assetnote.io/data/manual/raft-large-directories.txt
-wget https://wordlists-cdn.assetnote.io/data/automated/httparchive_apiroutes_2022_05_28.txt
-
-cd /root
-
-#SecList
-cd /root/Wordlist
-wget -c https://github.com/danielmiessler/SecLists/archive/master.zip -O SecList.zip \
-  && unzip SecList.zip \
-  && rm -f SecList.zip
- cd /root
-
-echo '####### INSTALAÇÃO REALIZADA COM SUCESSO #######'
-date
